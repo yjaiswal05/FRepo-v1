37a01d8b3580e8c3afe5dc2826a82c62b6a446a6
Yash Jaiswal <yjaiswal0511@gmail.com>
2024-11-29 23:03:38 +0530

    day 9 changes

--- server/src/routes/movieRoutes.js
+++ server/src/routes/movieRoutes.js
@@ -1,19 +1,14 @@
 import express from 'express';
 import { MovieController } from '../controllers/movieController.js';
-import { auth } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 const movieController = new MovieController();
 
-// Basic movie routes
+// GET routes
 router.get('/', movieController.getMovies);
-router.get('/:id', movieController.getMovie);
-router.get('/:id/reviews', movieController.getMovieReviews);
-router.get('/hero-images', movieController.getHeroImages);
 
-// Protected routes (require authentication)
-router.post('/:id/reviews', auth, movieController.createReview);
-router.put('/reviews/:reviewId', auth, movieController.updateReview);
-router.delete('/reviews/:reviewId', auth, movieController.deleteReview);
+// POST routes - make sure these have proper callback functions
+router.post('/:id/favorite', movieController.toggleFavorite);
+router.post('/:id/watchlist', movieController.toggleWatchlist);
 
 export default router; 
